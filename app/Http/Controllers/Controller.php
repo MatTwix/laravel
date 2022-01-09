8df6e1b319947f762c7af0ab7fd563bb4543aeda
MatTwix <mfgolden@yandex.ru>
2022-01-09 19:31:12 +0300

    third commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Faker\Factory;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +11,22 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function getNews()
+    {
+        $faker = Factory::create();
+
+        $news = [];
+
+        for ($i = 0; $i, $i < 10; $i++) {
+            $news = [
+                'id' => $i,
+                'title' => $faker->jobTitle(),
+                'description' => $faker->text(250),
+                'author' => $faker->userName()
+            ];
+        }
+
+        return $news;
+    }
 }
